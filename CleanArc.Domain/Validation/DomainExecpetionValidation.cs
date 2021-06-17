5a958d3f15c86bc640ea410ad0b8d8e08a0d3c75
andre meira <andre._.meira@hotmail.com>
2021-06-17 15:46:33 -0400

    Revert "Revert "add Public Class""
    This reverts commit 74b4197a2fa1091a397de40a758558631cdc1cbb.

--- CleanArc.Domain/Validation/DomainExecpetionValidation.cs
+++ CleanArc.Domain/Validation/DomainExecpetionValidation.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CleanArc.Domain.Validation
 {
-	class DomainExecpetionValidation : Exception
+	public class DomainExecpetionValidation : Exception
 	{
 		public DomainExecpetionValidation(string error) : base(error) { }
 
