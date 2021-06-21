7ad3ab7c351b1eead90fcbb6efd35599e5c39fbf
andre meira <andre._.meira@hotmail.com>
2021-06-21 17:18:08 -0400

    Add Services

---
+++ CleanArch.Application/Services/CategoryService.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using CleanArc.Domain.Entities;
+using CleanArc.Domain.Interfaces;
+using CleanArch.Application.DTOs;
+using CleanArch.Application.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArch.Application.Services
+{
+	class CategoryService : ICategoryServices
+	{
+		private  ICategoryRepository _categoryRepository;
+		private readonly IMapper _mapper;
+
+		public CategoryService(ICategoryRepository categoryRepository, IMapper mapper) 
+		{
+			_categoryRepository = categoryRepository;
+			_mapper = mapper;		
+		}
+
+		public async Task<IEnumerable<CategoryDTO>> GetCategories()
+		{
+			var categoreisEntity =await  _categoryRepository.GetCategoriesAsync();
+			return _mapper.Map<IEnumerable<CategoryDTO>>(categoreisEntity);
+		}
+
+		public async Task<CategoryDTO> GetById(int? id)
+		{
+			var categoryEntity = await _categoryRepository.GetByIdAsync(id);
+			return _mapper.Map<CategoryDTO>(categoryEntity);
+
+		}
+
+		public async Task Add(CategoryDTO categoryDTO)
+		{
+			var categoryEntity = _mapper.Map<Category>(categoryDTO);
+			await _categoryRepository.CreateAsync(categoryEntity);
+		}
+
+		public async Task Update(CategoryDTO categoryDTO)
+		{
+			var categoryEntity = _mapper.Map<Category>(categoryDTO);
+			await _categoryRepository.UpdateAsync(categoryEntity);
+		}
+
+		public async Task Remove(int? id)
+		{
+			var categoryEntity = _categoryRepository.GetByIdAsync(id).Result;
+			await _categoryRepository.RemoveAsync(categoryEntity);
+		}
+	}
+}
---
+++ CleanArch.Application/Services/ProcutServices.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using CleanArc.Domain.Entities;
+using CleanArc.Domain.Interfaces;
+using CleanArch.Application.DTOs;
+using CleanArch.Application.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArch.Application.Services
+{
+	class ProcutServices : IProductServices
+	{
+		private IProductRepository _productRepository;
+		private readonly IMapper _mapper;
+
+		public ProcutServices(IProductRepository productRepository, IMapper mapper) 
+		{
+			_productRepository = productRepository;
+			_mapper = mapper;
+		}
+
+		public async Task<ProductDTO> GetById(int? id)
+		{
+			var categoryEntity = await _productRepository.GetByIdAsync(id);
+			return _mapper.Map<ProductDTO>(categoryEntity);
+		}
+
+		public async Task<IEnumerable<ProductDTO>> GetProducts()
+		{
+			var categoreisEntity = await _productRepository.GetCategoriesAsync();
+			return _mapper.Map<IEnumerable<ProductDTO>>(categoreisEntity);
+		}
+
+		public async Task<ProductDTO> GetProductCategory(int? id)
+		{
+			var categoryEntity = await _productRepository.getProductCategoryAsync(id);
+			return _mapper.Map<ProductDTO>(categoryEntity);
+		}
+
+		public async Task Add(ProductDTO product)
+		{
+			var categoryEntity = _mapper.Map<Product>(product);
+			await _productRepository.CreateAsync(categoryEntity);
+		}
+
+
+		public async Task Update(ProductDTO product)
+		{
+			var categoryEntity = _mapper.Map<Product>(product);
+			await _productRepository.UpdateAsync(categoryEntity);
+		}
+
+		public async Task Remove(int? id)
+		{
+			var categoryEntity = _productRepository.GetByIdAsync(id).Result;
+			await _productRepository.RemoveAsync(categoryEntity);
+		}
+	}
+}
