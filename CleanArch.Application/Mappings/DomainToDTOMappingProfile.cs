9a6b55e7de2c92c01bf52cf4c9d18b9e4f0a889a
andre meira <andre._.meira@hotmail.com>
2021-06-21 17:17:18 -0400

    Configuran AutoMapper

---
+++ CleanArch.Application/Mappings/DomainToDTOMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CleanArc.Domain.Entities;
+using CleanArch.Application.DTOs;
+
+namespace CleanArch.Application.Mappings
+{
+	public class DomainToDTOMappingProfile : Profile
+	{
+		public DomainToDTOMappingProfile() 
+		{
+			CreateMap<Product, ProductDTO>().ReverseMap();
+			CreateMap<Category, CategoryDTO>().ReverseMap();
+		}
+	}
+}
