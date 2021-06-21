9be7e8463b3998be970bb3de6ff5be945568f507
andre meira <andre._.meira@hotmail.com>
2021-06-21 17:17:57 -0400

    add Interface Application

---
+++ CleanArch.Application/Interfaces/ICategoryServices.cs
@@ -0,0 +1,15 @@
+﻿using CleanArch.Application.DTOs;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArch.Application.Interfaces
+{
+	public interface ICategoryServices
+	{
+		Task<IEnumerable<CategoryDTO>> GetCategories();
+		Task<CategoryDTO> GetById(int? id);
+		Task Add(CategoryDTO categoryDTO);
+		Task Update(CategoryDTO categoryDTO);
+		Task Remove(int? id);
+	}
+}
---
+++ CleanArch.Application/Interfaces/IProductServices.cs
@@ -0,0 +1,18 @@
+﻿using CleanArch.Application.DTOs;
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArch.Application.Interfaces
+{
+	interface IProductServices 
+	{
+		public Task<ProductDTO> GetById(int? id);
+		public Task<IEnumerable<ProductDTO>> GetProducts();
+		public Task<ProductDTO> GetProductCategory(int? id);
+		public Task Add(ProductDTO product);
+		public Task Remove(int? id);
+		public Task Update(ProductDTO product);
+
+	}
+}
