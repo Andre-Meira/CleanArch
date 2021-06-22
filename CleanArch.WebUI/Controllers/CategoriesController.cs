78431f4258071494c810b9cda96a104a98193182
andre meira <andre._.meira@hotmail.com>
2021-06-22 17:18:36 -0400

    Config Index Controller

---
+++ CleanArch.WebUI/Controllers/CategoriesController.cs
@@ -0,0 +1,26 @@
+﻿using CleanArch.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArch.WebUI.Controllers
+{
+	public class CategoriesController : Controller
+	{
+		private ICategoryServices _categoryServices;
+
+		public CategoriesController(ICategoryServices categoryServices)
+		{
+			_categoryServices = categoryServices;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Index()
+		{
+			var categoreis = await _categoryServices.GetCategoriesAsync();
+			return View(categoreis);
+		}
+	}
+}
---
+++ CleanArch.WebUI/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using CleanArch.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArch.WebUI.Controllers
+{
+	public class ProductsController : Controller
+	{
+		private readonly IProductServices _productServices;
+
+		public ProductsController(IProductServices productService) 
+		{
+			_productServices = productService;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Index()
+		{
+			var products = await  _productServices.GetProductsAsync();
+			return View(products);
+		}
+
+
+	}
+}
