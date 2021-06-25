db751dd5ca9c9eb7671076366156ec39777dc7a3
andre meira <andre._.meira@hotmail.com>
2021-06-25 05:18:20 -0400

    Querry

---
+++ CleanArch.Application/Products/Queries/GetProductByIdQuerry.cs
@@ -0,0 +1,22 @@
+﻿using CleanArc.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Application.Products.Queries
+{
+	public class GetProductByIdQuerry : IRequest<Product>
+	{
+		public int Id { get; set;}
+
+		public GetProductByIdQuerry(int id)
+		{
+			Id = id;
+		}
+
+
+	}
+}
---
+++ CleanArch.Application/Products/Queries/GetProductsQuerry.cs
@@ -0,0 +1,15 @@
+﻿using CleanArc.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Application.Products.Queries
+{
+	public class GetProductsQuerry : IRequest<IEnumerable<Product>>
+	{
+
+	}
+}
