c4d335c12badf67b1eeae478e04ba129f046f078
andre meira <andre._.meira@hotmail.com>
2021-06-25 05:17:48 -0400

    Add Maping DTO of Commands

---
+++ CleanArch.Application/Mappings/DTOToComanndMappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using CleanArch.Application.DTOs;
+using CleanArch.Application.Products.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Application.Mappings
+{
+	public class DTOToComanndMappingProfile : Profile
+	{
+		public DTOToComanndMappingProfile()
+		{
+			CreateMap<ProductDTO, ProductCreateCommand>();
+			CreateMap<ProductDTO, ProductUpdateCommand>();
+		}
+	}
+}
